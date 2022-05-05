cda2f0ecf8cc6ef5b4f8dde0c20c586a16e0846d
Heeeon <bbjoite09@naver.com>
2022-05-05 15:53:30 +0900

    Fix : 경로 오류  수정

--- src/elements/BottomTabs.js
+++ src/elements/BottomTabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Information from '../src/components/Information';
+import Information from '../components/Information';
 
 const Tab = createBottomTabNavigator();
 
