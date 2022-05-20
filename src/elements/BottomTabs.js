ea6453b946be99a1ec79d913d8737f738a3e173f
Heeeon <bbjoite09@naver.com>
2022-05-20 20:40:09 +0900

    Style : Activity 페이지 이미지 추가

--- src/elements/BottomTabs.js
+++ src/elements/BottomTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Information from '../components/Information';
+import Activity from '../components/Activity';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,7 +9,7 @@ const BottomTabs = () => {
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
       <Tab.Screen name="diary" component={Information} />
-      <Tab.Screen name="activity" component={Information} />
+      <Tab.Screen name="activity" component={Activity} />
       <Tab.Screen name="information" component={Information} />
     </Tab.Navigator>
   );
