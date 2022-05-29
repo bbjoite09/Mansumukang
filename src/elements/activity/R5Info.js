ce580831af14e4d382511f6d4f20f543030c55ba
Jungheon Kim <kjh990705@naver.com>
2022-05-29 16:23:54 +0900

    Feat : InfoImage 설명추가

--- src/elements/activity/R5Info.js
+++ src/elements/activity/R5Info.js
@@ -10,28 +10,58 @@ const R5Info = () => {
         <View style={{marginTop: '5%'}}>
           <Text style={styles.headerText}>1. Refuse</Text>
           <View style={styles.bodyContainer}>
-            <Text style={styles.bodyTop}>Refuse</Text>
-            <Text style={styles.bodyTop}>Refuse</Text>
+            <Text style={styles.bodyTop}>
+              Say no to things you do not need, such as disposable cups and
+              plastic straws.
+            </Text>
+            <Image
+              style={{width: '90%', height: '80%'}}
+              source={require('../../assets/activity/Refuse.png')}
+            />
           </View>
           <Text style={styles.headerText}>2. Reduce</Text>
           <View style={styles.bodyContainer}>
-            <Text style={styles.bodyTop}>Reduce</Text>
-            <Text style={styles.bodyTop}>Reduce</Text>
+            <Text style={styles.bodyTop}>
+              Purchasing only the essentials and buying products with little
+              packaging.
+            </Text>
+            <Image
+              style={{width: '90%', height: '80%'}}
+              source={require('../../assets/activity/Reduce.png')}
+            />
           </View>
           <Text style={styles.headerText}>3. Reuse</Text>
           <View style={styles.bodyContainer}>
-            <Text style={styles.bodyTop}>Reuse</Text>
-            <Text style={styles.bodyTop}>Reuse</Text>
+            <Text style={styles.bodyTop}>
+              Reusing something that can be reused.
+            </Text>
+            <Image
+              style={{width: '90%', height: '80%'}}
+              source={require('../../assets/activity/Reuse.png')}
+            />
           </View>
           <Text style={styles.headerText}>4. Recycle</Text>
           <View style={styles.bodyContainer}>
-            <Text style={styles.bodyTop}>Recycle</Text>
-            <Text style={styles.bodyTop}>Recycle</Text>
+            <Text style={styles.bodyTop}>
+              Recycle products that can be recycled.
+            </Text>
+            <Image
+              style={{width: '90%', height: '80%'}}
+              source={require('../../assets/activity/Recycle.png')}
+            />
           </View>
           <Text style={styles.headerText}>5. Rot</Text>
           <View style={styles.bodyContainer}>
-            <Text style={styles.bodyTop}>Rot</Text>
-            <Text style={styles.bodyTop}>Rot</Text>
+            <Text style={styles.bodyTop}>
+              In the case of food that cannot be recycled,
+            </Text>
+            <Text style={styles.bodyTop}>
+              collect only food and rot to make feed.
+            </Text>
+            <Image
+              style={{width: '90%', height: '80%'}}
+              source={require('../../assets/activity/Rot.png')}
+            />
           </View>
         </View>
       </ScrollView>
